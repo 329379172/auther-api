2045edcf42576dbba52ae485a8b98dff19ca1dfa
linfeiyang <lupengfei@gozap.com>
2019-03-29 12:08:38 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const api = require('../lib/api');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/login', async function(req, res) {
+  try {
+    let token = await api.login(req.body.username, req.body.password);
+    return res.json({code: 200, message: 'ok', data: token});
+  } catch (e) {
+    return res.json({code: 400, message: e.message});
+  }
+});
+
+router.post('/async', async function(req, res) {
+  try {
+    let token = req.query.token;
+    let data = await api.asyncData(token);
+    return res.json({code: 200, message: 'ok', data: data});
+  } catch (e) {
+    return res.json({code: 400, message: e.message});
+  }
+});
+
+router.post('/upload', async function(req, res) {
+  try {
+    let token = req.query.token;
+    await api.upload(token, req.body);
+    return res.json({code: 200, message: 'ok'});
+  } catch (e) {
+    return res.json({code: 400, message: e.message});
+  }
+});
+
+module.exports = router;
